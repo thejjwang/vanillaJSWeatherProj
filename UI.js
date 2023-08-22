3180222db9eb1a4a27b5271c4798c9c3a432ed91
thejjwang <jonathanjacobwang@gmail.com>
2023-08-22 16:02:48 -0700

    just needs to fix previous buttons

--- UI.js
+++ UI.js
@@ -1,1 +1,7 @@
 
+// const createButton = (previousSearch) => {
+
+//     buttonContainer.innerHTML += (`<button class="btn" id={userInput.value}>${userInput.value}</button>`)
+// }
+// // for each button in button container need to add event listeners to direct them to that locations weathers
+// buttonContainer.addEventListener('click', )
--- index.js
+++ index.js
@@ -17,9 +17,10 @@ const searchWeather = () => {
 }
 searchBtn.addEventListener("click", searchWeather);
 
+
 const createButton = (previousSearch) => {
 
-    buttonContainer.innerHTML += (`<button id={userInput.value}>${userInput.value}</button>`)
+    buttonContainer.innerHTML += (`<button class="btn2" id={userInput.value}>${userInput.value}</button>`)
 }
 // for each button in button container need to add event listeners to direct them to that locations weathers
 buttonContainer.addEventListener('click', )
